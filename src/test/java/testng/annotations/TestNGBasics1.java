cd34ba0ca480aedf8646415bca0ddeaed3417e8f
kelumsampath <edirisingheemks@gmail.com>
2020-09-14 11:17:29 +0530

    introduced test annotations

--- src/test/java/testng/annotations/TestNGBasics1.java
+++ src/test/java/testng/annotations/TestNGBasics1.java
@@ -1,7 +1,13 @@
 package testng.annotations;
 
+import org.junit.Test;
+
 public class TestNGBasics1 {
     public static void main(String[] args) {
         System.out.println(" Hello! TestNG..");
+    }
+    @Test
+    public void print(){
+        System.out.println("Hello! tester..");
     }
 }
